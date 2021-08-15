507dab2d2f05befdf0e4e0e80f1391e127dd9642
Nathaniel Dauda Wobin <42215652+wobin1@users.noreply.github.com>
2021-08-15 06:24:57 +0100

    deleted the static and template folders

--- static/src/scripts/vendor.js
+++
@@ -1,9 +0,0 @@
-const question = document.getElementById('question')
-const optionA = document.getElementById('btn-optionA')
-const optionB = document.getElementById('btn-optionB')
-const optionC = document.getElementById('btn-optionC')
-const ans = document.getElementById('answer')
-
-const responseA = document.querySelector('#btn-optionA')
-const responseB = document.querySelector('#btn-optionB')
-const responseC = document .querySelector('#btn-optionC')
